1b95c9730a836b510e025619df7c2d52f7006ac9
hadley <1477546184@qq.com>
2016-09-18 18:33:24 +0800

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,17 +13,52 @@ module.exports = function(grunt){
 				]
 			}
 		},
+		less: {
+			transform: {
+				files: [
+					{
+						expand: true,
+						cwd: 'public/less',
+						src: '*.less',
+						dest: 'public/css',
+						ext: '.css'
+					}
+				]
+			}
+		},
+		cssmin: {
+			target: {
+				files: [
+					{
+						expand: true,
+						cwd: 'public/css',
+						src: '*.css',
+						dest: 'public/css',
+						ext: '.min.css'
+					}
+				]
+			}
+		},
 		watch: {
-			cjsx: {
-				files: 'public/jsx/*.jsx',
-				tasks: ['cjsx:transform'],
+			// cjsx: {
+			// 	files: 'public/jsx/*.jsx',
+			// 	tasks: ['cjsx:transform'],
+			// 	options: {
+			// 		debounceDelay: 250
+			// 	}
+			// }
+			less: {
+				files: 'public/less/*.less',
+				tasks: ['less:transform'],
 				options: {
 					debounceDelay: 250
 				}
 			}
 		}
 	});
-	grunt.loadNpmTasks('grunt-coffee-react');
+	// grunt.loadNpmTasks('grunt-coffee-react');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default', ['cjsx', 'watch']);
+	grunt.registerTask('default', ['less', 'cssmin', 'watch']);
 }
