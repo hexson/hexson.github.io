ba76d652211ddbdc084ae6d1d3c8ff00f99cdd48
hexson <1477546184@qq.com>
2016-09-30 02:22:46 +0800

    Js

--- js/containers/All.js
+++ js/containers/All.js
@@ -6,12 +6,31 @@ import Get from '../components/Get.js';
 
 
 export default class All extends Component {
+	constructor (props){
+		super(props);
+		this.state = {
+			list: [1]
+		}
+	}
+	loadBefore (){
+		let list = this.state.list.slice(0);
+		list.push(list[list.length-1]+1);
+		console.log(this.state.list,list)
+		this.setState({
+			list: list
+		})
+	}
 	render (){
 		return (
 			<div className="content">
 				<Header />
-				<div className="all-con">
-					<Get perpage="10" page="1" />
+				<div className="all-con ac">
+					{
+						this.state.list.map((v,i) => 
+							<Get key={i} perpage="2" page={v} />
+						)
+					}
+					<button className="load-before block f18" onClick={this.loadBefore.bind(this)}>加载更早的文章</button>
 				</div>
 			</div>
 		)
