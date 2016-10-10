928d5ef31f0657001dd25186cea314aad91c6df7
hexson <1477546184@qq.com>
2016-10-11 00:07:33 +0800

    update search module Js

--- js/containers/Search.js
+++ js/containers/Search.js
@@ -6,54 +6,6 @@ import Query from '../components/Query.js';
 
 
 export default class Search extends Component {
-	// constructor (props){
-	// 	super(props);
-	// 	this.loadBefore = this.loadBefore.bind(this);
-	// 	this.changeBtn = this.changeBtn.bind(this);
-	// 	this.search = this.search.bind(this);
-	// 	this.valueCallback = this.valueCallback.bind(this);
-	// 	this.state = {
-	// 		list: [],
-	// 		isNextPageView: true,
-	// 		btnClass: 'load-before block f18 none',
-	// 		keyword: null
-	// 	}
-	// }
-	// componentDidMount (){
-	// 	this.props.params.keyword = '';
-	// 	console.log(this.props.params.keyword);
-	// }
-	// valueCallback (){
-	// 	// this.refs.keywordInput.value = this.props.params.keyword || '';
-	// }
-	// loadBefore (){
-	// 	this.setState({
-	// 		btnClass: 'load-before block f18 load-before-animation'
-	// 	});
-	// 	setTimeout(() =>
-	// 		this.setState({
-	// 			btnClass: 'load-before block f18'
-	// 		})
-	// 	, 400);
-	// }
-	// changeBtn (){
-	// 	this.setState({
-	// 		isNextPageView: false,
-	// 		btnClass: 'load-before block f18'
-	// 	});
-	// }
-	// search (e){
-	// 	if (e.keyCode == 13){
-	// 		let keyword = this.refs.keywordInput.value;
-	// 		this.setState({
-	// 			keyword: keyword
-	// 		});
-	// 		window.location.href = '#/search/' + keyword;
-	// 	}
-	// }
-	reset (){
-		console.log(1);
-	}
 	render (){
 		return (
 			<div className="content">
