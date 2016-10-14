9892a3b609c673f952d19cff281dc94378ecc55d
hexson <1477546184@qq.com>
2016-10-14 14:53:41 +0800

    update article module Js

--- js/containers/Article.js
+++ js/containers/Article.js
@@ -26,7 +26,10 @@ export default class Article extends Component {
 			Issues.prototype.componentDidMount(this);
 		}
 		// this.refs.content.innerHTML = marked(this.refs.content.body);
-		console.log(this.refs.content)
+		// console.log(this.refs.content)
+	}
+	componentDidUpdate (){
+		this.refs.content.innerHTML = marked(this.refs.content.getAttribute('data-body'))
 	}
 	render (){
 		if (this.state.issues === null){
