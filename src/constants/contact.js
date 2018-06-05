afc337238604a150552ba6b1985ee83a5f633194
hexson <1477546184@qq.com>
2018-06-05 19:21:38 +0800

    add contact const

---
+++ src/constants/contact.js
@@ -0,0 +1,30 @@
+const githubId = 'hexson';
+
+const weiboId = '5176694848';
+
+const qq = '1477546184';
+
+const contact = [
+    {
+        name: 'Github',
+        icon: 'github',
+        url: `https://github.com/${githubId}`
+    },
+    {
+        name: 'Weibo',
+        icon: 'weibo',
+        url: `http://weibo.com/u/${weiboId}`
+    },
+    {
+        name: 'QQ',
+        icon: 'qq',
+        url: `tencent://message/?uin=${qq}&Menu=yes`
+    },
+    {
+        name: 'Gmail',
+        icon: 'envelope',
+        url: `mailto:hexson.f2e@Gmail.com`
+    }
+];
+
+export default contact;
